bc27e0b428e653da93c518627ab0a4288131050d
Navaneeth PV <navaneethpv550@gmail.com>
2025-09-28 22:10:36 +0300

    refactor(header): Remove unused search icon
    Removes the non-functional search (magnifying glass) icon from the header component. This icon was a placeholder and has been removed to clean up the UI.

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -185,20 +185,6 @@ export default function Header() {
                   d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
-              <svg
-                className="w-5 h-5 text-gray-400 hover:cursor-pointer"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
             </div>
           </div>
           <div
