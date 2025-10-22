113c1a62cba1234a5b7c8f63dfa3c773efeb68a2
Navaneeth PV <navaneethpv550@gmail.com>
2025-10-22 22:51:13 +0300

    Refactor: Adjust home page component import paths for structure
    Update import paths for Hero, Features, and Footer components in `src/app/page.tsx`. This aligns with a new, more organized component directory structure, placing each component in its own folder.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/home/components/Navigation/Header";
-import Hero from "@/components/home/Hero";
-import Features from "@/components/home/Features";
+import Hero from "@/components/home/components/Hero/Hero";
+import Features from "@/components/home/components/Features/Features";
 import BestDeals from "@/components/home/components/BestDeals/BestDeals";
 import MostViewed from "@/components/home/MostViewed";
 import Categories from "@/components/home/components/Catergoris/CategoriesSection";
 import Promotions from "@/components/home/Promotions";
-import Footer from "@/components/home/Footer";
+import Footer from "@/components/home/components/Footer/Footer";
 
 export default function Home() {
   return (
