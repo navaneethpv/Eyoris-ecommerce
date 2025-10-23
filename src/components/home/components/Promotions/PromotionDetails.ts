29d3d7f6b8c1688e1beff7beb0c83fa5e7fda2d6
Navaneeth PV <navaneethpv550@gmail.com>
2025-10-23 20:34:41 +0300

    feat: Update promotion details and image sources
    Update promotion titles and image URLs in `PromotionDetails.ts`. Replace outdated Flipkart image links with modern Pixabay, Unsplash, and iStock sources, introducing new themes like Smart Home Devices and Self-Care.

--- src/components/home/components/Promotions/PromotionDetails.ts
+++ src/components/home/components/Promotions/PromotionDetails.ts
@@ -1,64 +1,64 @@
 const promotions = [
   {
     id: "promo-petcare-7",
-    title: "Sicons Pet Care: Shampoos & Toys",
+    title: "Smart Home Devices: Alexa & More",
     items: [
-      "http://img5a.flixcart.com/image/pet-shampoo/r/j/5/sh-df-14-sicons-500-1100x1100-imaeh3hfvav85tva.jpeg",
-      "http://img5a.flixcart.com/image/pet-shampoo/r/j/5/sh-df-14-sicons-500-original-imaeh3hfvav85tva.jpeg",
-      "http://img6a.flixcart.com/image/pet-shampoo/c/c/d/sh-df-07-sicons-200-1100x1100-imaeh3kvn25fznjt.jpeg",
-      "http://img5a.flixcart.com/image/pet-shampoo/r/j/5/sh-df-14-sicons-500-original-imaeh3hfvav85tva.jpeg"
+      "https://cdn.pixabay.com/photo/2020/01/11/18/13/alexa-4758340_640.jpg",
+      "https://cdn.pixabay.com/photo/2022/01/21/15/11/lightbulb-6955039_640.jpg",
+      "https://cdn.pixabay.com/photo/2017/06/18/23/10/cctv-2417559_640.jpg",
+      "https://cdn.pixabay.com/photo/2019/09/02/15/43/smarthome-4447519_1280.jpg"
     ]
   },
-    {
-      id: "promo-home-2",
-      title: "FabHomeDecor: Sofa Beds & Home Accents",
-      items: [
-        "http://img6a.flixcart.com/image/sofa-bed/j/f/y/fhd112-double-foam-fabhomedecor-leatherette-black-leatherette-1100x1100-imaeh3gemjjcg9ta.jpeg",
-        "http://img5a.flixcart.com/image/artificial-plant/g/s/n/bbajc218-bengal-blooms-original-imaeg8ety3zfcnvw.jpeg",
-        "http://img5a.flixcart.com/image/vanity-box/8/5/t/j-635-bulaky-vanity-case-1100x1100-imadzy6atzfswnag.jpeg",
-        "http://img5a.flixcart.com/image/bottle/j/m/m/av004bgr-freelance-350-vacuum-bottles-1000x1100-imaegykdk6ytzrzz.jpeg"
-      ]
-    },
-    {
-      id: "promo-footwear-3",
-      title: "AW Bellies & Footwear Collection",
-      items: [
-        "http://img5a.flixcart.com/image/shoe/7/z/z/red-as-454-aw-11-original-imaeebfwsdf6jdf6.jpeg",
-        "http://img6a.flixcart.com/image/shoe/b/p/n/pink-200db202-dilli-bazaaar-10-original-imaeh2zz4x6hnuwf.jpeg",
-        "http://img5a.flixcart.com/image/shoe/s/g/m/black-r998-22-ladela-38-original-imaega8phqh6tf4e.jpeg",
-        "http://img5a.flixcart.com/image/shoe/p/f/h/black-sfwf0377-style-foot-44-original-imaeh4cxasyrdtjr.jpeg"
-      ]
-    },
-    {
-      id: "promo-petcare-4",
-      title: "Sicons Pet Care: Shampoos & Toys",
-      items: [
-      "http://img5a.flixcart.com/image/pet-shampoo/r/j/5/sh-df-14-sicons-500-1100x1100-imaeh3hfvav85tva.jpeg",
-      "http://img5a.flixcart.com/image/pet-shampoo/r/j/5/sh-df-14-sicons-500-original-imaeh3hfvav85tva.jpeg",
-      "http://img6a.flixcart.com/image/pet-shampoo/c/c/d/sh-df-07-sicons-200-1100x1100-imaeh3kvn25fznjt.jpeg",
-      "http://img5a.flixcart.com/image/pet-shampoo/v/x/m/sh-df-02-sicons-200-original-imaeh3kjba4htqfg.jpeg"
+  {
+    id: "promo-home-2",
+    title: "Work From Home Essentials: Decor & More",
+    items: [
+      "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_640.jpg",
+      "https://cdn.pixabay.com/photo/2016/11/19/16/01/audio-1840073_640.jpg",
+      "https://cdn.pixabay.com/photo/2016/06/28/05/10/laptop-1483974_640.jpg",
+      "https://media.istockphoto.com/id/155352786/photo/office-chair.jpg?s=2048x2048&w=is&k=20&c=8CWTKNfUOZU4fiF6_nj_fVJrvP0ZGyVQVcLXvYRiwIc="
+    ]
+  },
+  {
+    id: "promo-footwear-3",
+    title: "Trendy Footwear Collection: Bellies & More",
+    items: [
+      "https://cdn.pixabay.com/photo/2017/04/08/16/16/kicks-2213619_640.jpg",
+      "https://cdn.pixabay.com/photo/2020/12/21/13/50/bath-towels-5849999_640.jpg",
+      "https://cdn.pixabay.com/photo/2017/07/02/19/24/dumbbells-2465478_640.jpg",
+      "https://cdn.pixabay.com/photo/2016/03/27/19/31/fashion-1283863_640.jpg"
+    ]
+  },
+  {
+    id: "promo-petcare-4",
+    title: "Self-Care & Skincare: Pamper Yourself",
+    items: [
+      "https://images.unsplash.com/photo-1551446339-1e5c6f164ec2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fExvdGlvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500",
+      "https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNraW5jYXJlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500",
+      "https://images.pexels.com/photos/3851905/pexels-photo-3851905.jpeg",
+      "https://cdn.pixabay.com/photo/2023/11/14/23/06/beauty-8388873_640.jpg  "
     ]
   },
   {
     id: "promo-lifestyle-5",
-      title: "Freelance Bottles & Shopmania Stationery",
-      items: [
-        "http://img5a.flixcart.com/image/bottle/j/m/m/av004bgr-freelance-350-vacuum-bottles-1000x1100-imaegykdk6ytzrzz.jpeg",
-        "http://img5a.flixcart.com/image/diary-notebook/g/y/y/shopmania-nb00664-1100x1100-imaej4wguh2c6dfb.jpeg",
-        "http://img5a.flixcart.com/image/diary-notebook/e/g/h/shopmania-nb00678-1100x1100-imaej4whhbgzn5z3.jpeg",
-        "http://img5a.flixcart.com/image/packaging-security-bag/6/y/q/8-x-10-inches-security-bags-without-pod-jacket-courier-bag-1100x1100-imaegbjpxtthuyzc.jpeg"
-      ]
-    },
-    {
-      id: "promo-kids-6",
-      title: "Oye & Dongli Kids' Apparel",
-      items: [
-        "http://img6a.flixcart.com/image/dungaree-romper/9/z/d/bpr0015-oye-1-2-years-original-imaeeyu6ashttgyv.jpeg",
-        "http://img5a.flixcart.com/image/t-shirt/w/x/t/dlhbb445-beige-black-gyellow-purple-dongli-10-11-years-original-imaehb54gxhchxcd.jpeg",
-        "http://img5a.flixcart.com/image/t-shirt/f/n/f/dlhbb445-beige-black-gyellow-purple-dongli-15-16-years-original-imaehb55kjd6g4kg.jpeg",
-        "http://img6a.flixcart.com/image/t-shirt/g/y/5/dlhbb445-beige-black-gyellow-purple-dongli-12-13-years-original-imaehb54re656maf.jpeg"
-      ]
-    }
-  ];
+    title: "Streetwear Fashion: Urban Style & More",
+    items: [
+      "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg",
+      "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg",
+      "https://images.unsplash.com/photo-1653704841996-c2ed854aedd8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fwc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500",
+      "https://images.pexels.com/photos/189199/pexels-photo-189199.jpeg"
+    ]
+  },
+  {
+    id: "promo-kids-6",
+    title: "Eco-Friendly Living: Sustainable Products",
+    items: [
+      "https://cdn.pixabay.com/photo/2017/08/06/23/10/flowers-2597372_640.jpg",
+      "https://cdn.pixabay.com/photo/2020/04/25/14/09/spring-5090978_640.jpg",
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/solar-light-set/n/u/g/2-solar-interaction-wall-lamp-bk-888-5-5-lavitra-original-imahfhdbsskzetcn.jpeg?q=70",
+      "https://media.istockphoto.com/id/611634162/photo/bamboo-wooden-tableware-isolated-on-white.webp?a=1&b=1&s=612x612&w=0&k=20&c=JF7wxDR10QUDfHdo67c4a-9UL2RiQdN5sTxZUc-fzpg="
+    ]
+  }
+];
 
   export default  promotions;
