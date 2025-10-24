b218a9fed8573f53acb8ff2f99cd893c1bd61dbd
Navaneeth PV <navaneethpv550@gmail.com>
2025-10-24 23:01:36 +0300

    Refactor: Extract product logic to hook and update promotions
    Move product fetching and processing logic from `MostViewed` component to a new `useMostViewed` hook for better separation of concerns. Update promotion details in `PromotionDetails.ts` with new image sources and a revised "Self-Care & Skincare" promotion. Add `images.pexels.com` to Next.js image domains to support new image assets.

--- src/components/home/components/MostViewed/MostViewed.tsx
+++ src/components/home/components/MostViewed/MostViewed.tsx
@@ -3,53 +3,15 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Product } from '@/types/index';
 import { useCart } from '@/context/CartContext';
+import mostViewed from '@/hooks/mostViewed';
 
-interface EyoristProduct {
-  uniq_id: string;
-  product_name: string;
-  retail_price: string;
-  discounted_price: string;
-  image: string[];
-  overall_rating: string;
-  product_rating: string;
-}
 
 export default function MostViewed() {
-  const [products, setProducts] = useState<Product[]>([]);
   const {addToCart} = useCart();
-
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await fetch('/sample_products.json');
-        const data: EyoristProduct[] = await response.json();
-
-            const processedProducts: Product[] = data.slice(25, 34).map((product) => {
-              const rating = product.product_rating === 'No rating available' ? 0 : parseFloat(product.product_rating);
-              // Ensure image is parsed correctly and is an array before accessing the first element
-              const imageUrl = Array.isArray(product.image) ? product.image[0] : (typeof product.image === 'string' ? JSON.parse(product.image)[0] : '');
-              return {
-                uniq_id: product.uniq_id,
-                name: product.product_name,
-                rating: rating,
-                reviews: 1000,
-                currentPrice: product.discounted_price ? product.discounted_price : 'N/A',
-                oldPrice: product.retail_price ? product.retail_price : 'N/A',
-                image: imageUrl,
-                discount: product.discounted_price && product.retail_price ? `${Math.round(((parseFloat(product.retail_price) - parseFloat(product.discounted_price)) / parseFloat(product.retail_price)) * 100)}%` : undefined,
-              } as Product;
-            });
-
-        setProducts(processedProducts);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    }
-
-    fetchProducts();
-  }, []);
-
   const [currentIndex, setCurrentIndex] = useState(0);
+  const products = mostViewed();
+
+  
 
   const handleNext = () => {
     if (currentIndex < products.length - 5) {
