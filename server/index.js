ca28bce2b820f88feb772915ed017e8f9baba50a
Navaneeth PV <navaneethpv550@gmail.com>
2025-11-29 19:04:06 +0300

    feat: Integrate MongoDB and Mongoose into the server setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db_connection");
+const route  = require("./router/porductRoute");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json());
 
+connectDB()
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use('/product', route);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
