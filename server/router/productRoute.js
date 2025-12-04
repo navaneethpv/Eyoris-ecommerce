e341223e8860b0a7e2f0a6436f62b3b9384fb4b9
Navaneeth PV <navaneethpv550@gmail.com>
2025-12-05 00:16:50 +0300

    feat(productRoute): Add route for retrieving most viewed products

--- server/router/productRoute.js
+++ server/router/productRoute.js
@@ -1,4 +1,5 @@
 // eyoris/server/router/porductRoute.js
+const { getMostViewed } = require("../controller/mostViewedController");
 const { addProduct, getProducts, getProductById, getBestDeals } = require("../controller/productController");
 const express = require("express");
 const router = express.Router();
@@ -8,6 +9,8 @@ router.post("/add", addProduct);
 
 // Best deals
 router.get("/best-deals", getBestDeals);
+
+router.get("/most-viewed", getMostViewed);
 
 // List with cursor pagination
 router.get("/", getProducts);
